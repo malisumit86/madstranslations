d2c539a6e6cbfdcd43c560ad282b2d52b4c87f48
Dhairya Deshmukh <dhairyadeshmukh75@gmail.com>
2022-04-06 18:20:36 +0530

    new: Debug False, added whitenoise

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -131,7 +131,6 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
